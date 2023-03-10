a03abcf614880885951b04e62c83f110a766b629
sophperepelit <sophperepelit@gmail.com>
2023-03-10 09:08:31 -0600

    Initial Commit

---
+++ AP CompSci Final/AP_CompSci_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_CompSci_FinalApp.swift
+//  AP CompSci Final
+//
+//  Created by Sophia Perepelitsyn23 on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AP_CompSci_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
